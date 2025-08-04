4f90a3b6c7b547d4b64f8508f1f3cffdd0aa532a
Marcelo Mutzbauer <Marcelo.Mutzbauer@stud.uni-regensburg.de>
2025-08-04 15:30:15 +0200

    Introduce the notion of an active subgraph

--- src/article/CompatibilityGraph.tsx
+++ src/article/CompatibilityGraph.tsx
@@ -7,82 +7,102 @@ import { BucketCanvas, computeNodeBucket, drawBuckets, genBucketsJsx, randomPoin
 import { Graph } from "graphlib";
 import { foreachNaryString } from "@/common/mathUtils";
 import { drawNode, GraphNodeType } from "@/common/NodeDrawing";
-import { midColor, topColor } from "@/common/Colors";
+import { bottomColor, midColor, topColor } from "@/common/Colors";
 
-function generateCompatGraph(cnv: BucketCanvas) {
-  let graph = cnv.graph;
-  let result: Graph = new Graph({multigraph: false, directed: false});
 
-  let buckets: GraphNode[][] = Array.from({ length: graph.cliqueSize }, () => []);
+class CompatGraph {
+  mkNodeId(bucketIdx: number, coloring: number[]) {
+    return bucketIdx.toString() + "-" + coloring.toString();
+  }
 
-  for (let node of graph.nodes) {
-    let bucketIdx = computeNodeBucket(graph, node);
+  constructor(cnv: BucketCanvas) {
+    let graph = cnv.graph;
+    let result: Graph = new Graph({ multigraph: false, directed: false });
 
-    if (bucketIdx < 0) {
-      continue;
+    this.buckets = Array.from({ length: graph.cliqueSize }, () => []);
+
+    for (let node of graph.nodes) {
+      let bucketIdx = computeNodeBucket(graph, node);
+
+      if (bucketIdx < 0) {
+        continue;
+      }
+
+      this.buckets[bucketIdx].push(node);
     }
 
-    buckets[bucketIdx].push(node);
-  }
+    // Next, iterate over all possible colorings and add nodes.
+    for (let bucketIdx = 0; bucketIdx < graph.cliqueSize; bucketIdx++) {
+      let bucket = this.buckets[bucketIdx];
 
-  // Next, iterate over all possible colorings and add nodes.
-  for (let bucketIdx = 0; bucketIdx < graph.cliqueSize; bucketIdx++) {
-    let bucket = buckets[bucketIdx];
-    
-    foreachNaryString(bucket.length, 3, (coloring: number[]) => {
-      let [x, y] = randomPointInBucket(cnv, bucketIdx);
+      foreachNaryString(bucket.length, 3, (coloring: number[]) => {
+        let [x, y] = randomPointInBucket(cnv, bucketIdx);
 
-      // Check if these ternary string defines a proper 3-coloring.
-      let proper = true;
-      for (let i = 0; i < bucket.length && proper; i++) {
-        for (let j = i + 1; j < bucket.length && proper; j++) {
-          if (coloring[i] != coloring[j] && graph.hasEdge(bucket[i], bucket[j])) {
-            proper = false;
-          }
-        }
-      }
-
-      result.setNode(bucketIdx.toString() + "-" + coloring.toString(), {x, y, proper, bucketIdx, coloring});
-      return false;
-    });
-  }
-
-  // Connect up the nodes.
-  let nodes = result.nodes();
-  for (let i = 0; i < nodes.length; i++) {
-    for (let j = i + 1; j < nodes.length; j++) {
-      let nodeA = nodes[i];
-      let nodeB = nodes[j];
-
-      let compatible = true;
-
-      let dataA = result.node(nodeA);
-      let dataB = result.node(nodeB);
-
-      if (dataA.bucketIdx === dataB.bucketIdx) {
-        compatible = false;
-      }
-
-      if (dataA.proper && dataB.proper && compatible) {
-        let bucketA = buckets[dataA.bucketIdx];
-        let bucketB = buckets[dataB.bucketIdx];
-
-        for (let i = 0; i < bucketA.length && compatible; i++) {
-          for (let j = 0; j < bucketB.length && compatible; j++) {
-            if (dataA.coloring[i] != dataB.coloring[j] && graph.hasEdge(bucketA[i], bucketB[j])) {
-              compatible = false;
+        // Check if these ternary string defines a proper 3-coloring.
+        let proper = true;
+        for (let i = 0; i < bucket.length && proper; i++) {
+          for (let j = i + 1; j < bucket.length && proper; j++) {
+            if (coloring[i] != coloring[j] && graph.hasEdge(bucket[i], bucket[j])) {
+              proper = false;
             }
           }
         }
-      }
 
-      if (compatible) {
-        result.setEdge(nodeA, nodeB);
+        result.setNode(this.mkNodeId(bucketIdx, coloring), { x, y, proper, bucketIdx, coloring });
+        return false;
+      });
+    }
+
+    // Connect up the nodes.
+    let nodes = result.nodes();
+    for (let i = 0; i < nodes.length; i++) {
+      for (let j = i + 1; j < nodes.length; j++) {
+        let nodeA = nodes[i];
+        let nodeB = nodes[j];
+
+        let compatible = true;
+
+        let dataA = result.node(nodeA);
+        let dataB = result.node(nodeB);
+
+        if (dataA.bucketIdx === dataB.bucketIdx) {
+          compatible = false;
+        }
+
+        if (dataA.proper && dataB.proper && compatible) {
+          let bucketA = this.buckets[dataA.bucketIdx];
+          let bucketB = this.buckets[dataB.bucketIdx];
+
+          for (let i = 0; i < bucketA.length && compatible; i++) {
+            for (let j = 0; j < bucketB.length && compatible; j++) {
+              if (dataA.coloring[i] != dataB.coloring[j] && graph.hasEdge(bucketA[i], bucketB[j])) {
+                compatible = false;
+              }
+            }
+          }
+        }
+
+        if (compatible) {
+          result.setEdge(nodeA, nodeB);
+        }
       }
     }
+
+    this.activeColorings = new Array(this.buckets.length).fill(null);
+    for (let bucketIdx = 0; bucketIdx < this.buckets.length; bucketIdx++) {
+      let coloring = new Array(this.buckets[bucketIdx].length).fill(0);
+      this.activeColorings[bucketIdx] = this.mkNodeId(bucketIdx, coloring);
+    }
+
+    this.graph = result;
   }
 
-  return result;
+  graph: Graph;
+  buckets: GraphNode[][];
+
+  // For each bucket, store a selected compatibility node.
+  // Contains the node IDs.
+  activeColorings: string[];
 }
 
 export default function CompatibilityGraph({
@@ -91,18 +111,18 @@ export default function CompatibilityGraph({
 {
   let cnv = useBucketCanvas(graph);
 
-  let [compatGraph, setCompatGraph] = useState<Graph>();
+  let [compatGraph, setCompatGraph] = useState<CompatGraph>();
 
   useEffect(() => {
-    setCompatGraph(generateCompatGraph(cnv));
+    setCompatGraph(new CompatGraph(cnv));
   }, [graphVersion]);
 
   function drawGraph(canvas: React.JSX.Element[], labels: React.JSX.Element[]) {
     drawBuckets(cnv, canvas, labels);
 
-    compatGraph?.edges().forEach(edge => {
-      let src = compatGraph.node(edge.v);
-      let tgt = compatGraph.node(edge.w);
+    compatGraph?.graph.edges().forEach(edge => {
+      let src = compatGraph.graph.node(edge.v);
+      let tgt = compatGraph.graph.node(edge.w);
 
 
       let [fromX, fromY] = cnv.grid.toScreen(src.x || 0, src.y || 0);
@@ -114,10 +134,15 @@ export default function CompatibilityGraph({
       canvas.push(line)
     })
 
-    compatGraph?.nodes().forEach(nodeId => {
-      let node = compatGraph.node(nodeId);
+    compatGraph?.graph.nodes().forEach(nodeId => {
+      let node = compatGraph.graph.node(nodeId);
+      let color = midColor;
 
-      drawNode(cnv.zoom, cnv.grid, GraphNodeType.Internal, midColor, node.x, node.y, canvas, labels);
+      if (compatGraph.activeColorings[node.bucketIdx] === nodeId) {
+        color = bottomColor;
+      }
+
+      drawNode(cnv.zoom, cnv.grid, GraphNodeType.Internal, color, node.x, node.y, canvas, labels);
     });
 
   }
