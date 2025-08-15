8e48ce9eec1036c10c08d815dde09a3b3d0de830
Marcelo Mutzbauer <Marcelo.Mutzbauer@stud.uni-regensburg.de>
2025-08-15 19:00:37 +0200

    Copy the topg-test page into the codebase

--- src/article/page.tsx
+++ src/article/page.tsx
@@ -2,16 +2,91 @@ import React, { useEffect, useLayoutEffect, useReducer, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '@/styles/index.css';
-import { PrimeReactProvider } from 'primereact/api';
-import "primereact/resources/themes/lara-dark-teal/theme.css";
 import { marked } from 'marked';
 import linkupMd from "./linkup.md";
+import { initMacros, KB, KI } from '@/common/katex';
+import { ChevronsRight } from 'lucide-react';
+
+export function TaggedBox({ tag, children, className } : { tag? : React.ReactNode, children : React.ReactNode, className? : string }) {
+  return <div className={`relative shadow-lg border border-gray-200 rounded-lg p-3 m-2 ${className}`}>
+    {tag && <div className="absolute text-2xl -left-5 -translate-x-full">{tag}</div>}
+    {children}
+  </div>;
+}
+
+export function Title({children} : {children : React.ReactNode}) {
+  return <>
+  <h1 className={`text-5xl font-bold mt-8 text-stone-800`}>{children}</h1>
+  <hr className="h-px my-8 bg-gray-400 border-0 mb-14" />
+  </>;
+}
+
+
+export function Section({children} : {children : React.ReactNode}) {
+  return <>
+  <h1 className={`text-3xl font-bold mt-14 mb-0 text-stone-800`}>{children}</h1>
+  <hr className="h-px my-8 bg-gray-200 border-0 mt-2 mb-4 " />
+  </>;
+}
+
+export function Strong({children} : {children : React.ReactNode}) {
+  return <span className="text-stone-800 font-bold">{children}</span>;
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-function Main() {
-  return <div>Hello World</div>;
+
+function Theorem({ id, name, children } : { id : string, name : React.ReactNode, children : React.ReactNode }) {
+  return <TaggedBox tag={<ChevronsRight className="w-6"/>}>
+    <div className={`text-2xl mb-1 mt-0 font-bold px-2 py-1`}>{name}</div>
+    <hr />
+    <div className="mt-2">
+    {children}
+    </div>
+  </TaggedBox>;
 }
+
+function Abstract() {
+  return <TaggedBox className="mb-10" tag={<>Abstract</>}>
+We continue our discussion of basic properties of topological groups initiated in Talk 1.
+  <br/>
+The most notable results will be the <a href="#thm-openmap">open mapping theorem</a>, a useful criterion for checking if a topological group morphism is open; 
+we will also study an important property of the circle group <KI>\T</KI> and the real line <KI>\R</KI> -- <Strong>divisibility</Strong>
+(<a href="#sec-divgrp">Section ?</a>); and finally,
+we will <Strong>classify</Strong> some of the <Strong>topological groups</Strong> that <q>look like</q> <KI>(\R^n, +)</KI> locally <a href="#thm-class-euclid">Theorem ?</a>
+<br/>
+Furthermore, we will deliver some results on the behavior of local compactness, products and quotients.
+  </TaggedBox>
+}
+
+function Article() {
+  return (
+    <>
+    <Title>Topological Groups, Part II</Title>
+    <Abstract />
+      
+We begin by harping a bit more on
+
+    <Section>Unit Neighborhoods</Section>
+
+    In our kick-off talk last week, we defined unit-neighborhoods in a topological group <KI>G</KI> as neighborhoods (which we do not require be open!) of the neutral element/unit/origin <KI>e \in G</KI>.
+    <br/>
+As we will see in this section, they provide a quick way of probing a topological group for a lot of properties. This section will be an (incomplete) assortment of some of those tests.
+<br/>
+It is good to think of these as the analogue of open balls in metric spaces. To make this parallel more precise, let <KI>V</KI> be a normed vectorspace and let <KB>{String.raw`B_\epsilon := \{x\in V : \|x\| < \epsilon\}`}</KB> denote an open <KI>\epsilon</KI>-ball around the origin. Then we have the
+
+<Theorem name="Lemma: Ball test for open subsets" id="thm-ball-opensubs">
+A subset <KI>U</KI> in <KI>V</KI> is open iff it can be expressed as a union of translates of the norm ball
+<KB>{String.raw`U=\Cup_{v\in U}(v+B_{\epsilon(v)}),`}</KB>
+for some collection of small <KI>{String.raw`\epsilon_v > 0, v \in V`}</KI> s.t. <KI>{String.raw`v+B_{\epsilon(v)} \subs U`}</KI>.
+</Theorem>
+
+<Section>Locally Isomorphic Groups</Section>
+The torus <KI>\T^2</KI> and the Euclidean plane <KI>\R^2</KI> are locally isomorphic, as demonstrated in the following picture:
+</>);
+}
+
 
 function parseMd() {
   console.log(marked(linkupMd));
@@ -20,14 +95,11 @@ function parseMd() {
 parseMd();
 
 // Disable for now. My macros break KaTeX's \neq (eyeroll), no idea why
-//initMacros();
+initMacros();
 root.render(
   <React.StrictMode>
-      <PrimeReactProvider>
-        {/* A fixed pos seems to be necessary to get rid of some strange scrollbars */}
-        <div className="fixed flex overflow-hidden h-dvh w-full">
-          <Main />
+        <div className="mx-auto max-w-4xl p-3 font-crimson text-lg">
+          <Article />
         </div>
-      </PrimeReactProvider>
   </React.StrictMode>
 );
--- src/route-benes/page.tsx
+++ src/route-benes/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '@/styles/index.css';
+import "primereact/resources/themes/lara-dark-teal/theme.css";
 
 import { useState } from "react";
 import BenesNet from "./BenesNet";
