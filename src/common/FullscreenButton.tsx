c828c000d6804a686caccff231df23cba6022862
Marcelo Mutzbauer <Marcelo.Mutzbauer@stud.uni-regensburg.de>
2025-12-09 21:11:02 +0100

    Make the fullscreen button slightly transparent if not hovered

--- src/common/FullscreenButton.tsx
+++ src/common/FullscreenButton.tsx
@@ -20,8 +20,9 @@ export function FullscreenButton() {
     }
   }
 
-  return <div className="fixed z-50 px-3 py-2 text-gray-800 rounded-xl shadow-lg bottom-4 right-4">
-          <Button icon={isFullscreen ? <Minimize2 size={18} /> : <Maximize size={18} />}
-        onClick={onClick} />
-      </div>;
+  return <div className="fixed z-50 px-3 py-2 text-gray-800 rounded-xl shadow-lg bottom-4 right-4 opacity-60 hover:opacity-100 transition-opacity">
+    <Button icon={isFullscreen ? <Minimize2 size={18} /> : <Maximize size={18} />}
+      className=""
+      onClick={onClick} />
+  </div>;
 }
